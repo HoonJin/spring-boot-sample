e3c68fea2223c45f1486d9931542770ce043d6ed
Daniel Ji <bwjhj1030@gmail.com>
2021-11-09 21:12:21 +0900

    UserService.findByEmail 유닛테스트 추가

--- src/test/java/com/sample/sample/service/UserServiceTest.java
+++ src/test/java/com/sample/sample/service/UserServiceTest.java
@@ -11,8 +11,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-;
-
 @SpringBootTest
 class UserServiceTest {
 
@@ -26,18 +24,35 @@ class UserServiceTest {
     }
 
     @Test
-    void createUser_successfully() {
+    void createUser_success() {
         String email = "test@test.com";
-        User user = userService.createUser("test@test.com");
+        User user = userService.createUser(email);
         assertThat(user.getEmail()).isEqualTo(email);
         assertThat(user.getId()).isNotNull();
     }
 
     @Test
-    void duplicateException() {
+    void createUser_duplicateException() {
         Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
             userService.createUser("test@test.com");
             userService.createUser("test@test.com");
+        });
+    }
+
+    @Test
+    void findByEmail_success() {
+        String email = "test@test.com";
+        userService.createUser(email);
+        User user = userService.findByEmail(email);
+
+        assertThat(user.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    void findByEmail_illegalArgumentException() {
+        String email = "not_exist@test.com";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.findByEmail(email);
         });
     }
 
