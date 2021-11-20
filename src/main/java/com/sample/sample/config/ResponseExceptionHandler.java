8906bbcc1037eac9ba1ea9c253675cb1d1829d01
Daniel Ji <bwjhj1030@gmail.com>
2021-11-20 16:47:34 +0900

    엔티티로 잘못들어가있던 response object는 다른곳에서 쓸일이 없는 클래스이므로 에러 핸들러 안에 스태틱 클래스로 만듬

--- src/main/java/com/sample/sample/config/ResponseExceptionHandler.java
+++ src/main/java/com/sample/sample/config/ResponseExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.sample.sample.config;
 
-import com.sample.sample.entity.general.ErrorResponse;
 import com.sample.sample.exception.CustomException;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -37,4 +38,32 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, res, new HttpHeaders(), SERVICE_UNAVAILABLE, request);
     }
 
+    @ToString
+    @Getter
+    private static class ErrorResponse {
+
+        private String code;
+        private String message;
+
+        public static ErrorResponse of(CustomException ex) {
+            ErrorResponse res = new ErrorResponse();
+            res.code = ex.getCode();
+            res.message = ex.getMessage();
+            return res;
+        }
+
+        public static ErrorResponse of(RuntimeException ex) {
+            ErrorResponse res = new ErrorResponse();
+            res.code = "internal_server_error";
+            res.message = "please contact to service provider.";
+            return res;
+        }
+
+        public static ErrorResponse of(Exception ex) {
+            ErrorResponse res = new ErrorResponse();
+            res.code = "unavailable_service";
+            res.message = "please contact to service provider.";
+            return res;
+        }
+    }
 }
